09ff1b16751e3e343873e4ed00493396834f3039
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2024-03-26 14:51:17 +0100

    Added regular matrix for the gameboard, next step is to create the method to initiliaze the gameboard

--- src/main/java/com/denniseckerskorn/threeinarow/ThreeInARow.java
+++ src/main/java/com/denniseckerskorn/threeinarow/ThreeInARow.java
@@ -2,14 +2,24 @@ package com.denniseckerskorn.threeinarow;
 
 //Class for the game Logic
 public class ThreeInARow {
+    private final int rows;
+    private final int columns;
     private final Player player1;
     private final Player player2;
     private final int rounds;
+    private char[][] gameBoard;
 
-    public ThreeInARow(Player player1, Player player2, int rounds) {
+
+    public ThreeInARow(int rows, int columns, Player player1, Player player2, int rounds) {
+        this.rows = rows;
+        this.columns = columns;
         this.player1 = player1;
         this.player2 = player2;
         this.rounds = rounds;
+        gameBoard = new char[rows][columns];
+        /*
+        initializeGameBoard();
+         */
     }
 
 
